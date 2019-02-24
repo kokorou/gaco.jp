81882a13e2cd95876276d4ff401f39f0da12a654
xubingyang <hi@xubingyang.com>
2019-02-24 17:04:52 +0900

    fix links and update assets

--- assets/php/form-processor.php
+++
@@ -1,4 +0,0 @@
-<div class="alert alert-danger alert-dismissible fade show" role="alert">
-			<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-			There is no <strong> "to" </strong> field found in the form. Please follow the documentation.
-		</div>
